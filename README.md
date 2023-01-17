7bf2b93585a8cca89e465cba10926e704ca0a69a
ashley <alandis01@gmail.com>
2023-01-17 18:20:26 -0500

    updated readme, added visuals and complete table of contents

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# notetaker
+# Notetaker
+
+[Link to my project](https://fierce-mesa-56296.herokuapp.com)
+
+## Table of Contents 
+-[Description of Project](#description-of-project)
+-[Installation](#installation)
+-[Visuals](#visuals)
+-[License](#license)
+-[Contributing](#contributing)
+-[Questions](#questions)
+
+## Description
+I have created a notetaker application that allows you to navigate directly to a "Get Started" page. Once you click the Get Started button, you will then be directed to a page that will allow you to record and save typed notes. 
+
+## Installation
+npm init -y required to install package.json
+npm install required to install dependencies 
+
+## Visuals
+![Notetaker 1](./Images/Notetaker%201.png)
+![Notetaker 2](./Images/Notetaker%202.png)
+![Notetaker 3](./Images/Notetaker%203.png)
+
+## License
+N/A
+
+## Contributing
+N/A
+
+## Tests
+N/A
+
+## Questions
+If you have any other questions, please visit my GitHub [repository](https://github.com/alandis01/notetaker)
--- server.js
+++ server.js
@@ -4,17 +4,11 @@ const PORT = process.env.PORT || 3001;
 const path = require('path');
 const app = express();
 
-// app.get('/', (req, res) => {
-//     res.send('Note Taker');
-// });
-
 const notes = require("./db/db.json");
-// const { networkInterfaces } = require('os');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-// app.use(apiRoutes);
 
 const uniqueId = require('unique-id');
 
@@ -48,43 +42,6 @@ app.post('/api/notes', (req, res) => {
   const note = newNote(req.body, notes);
   res.json(note);
 });
-
-// const readAndAppend = (content, file) => {
-//     fs.readFile(file, 'utf8', (err, data) => {
-//       if (err) {
-//         console.error(err);
-//       } else {
-//         const parsedData = JSON.parse(data);
-//         parsedData.push(content);
-//         writeToFile(file, parsedData);
-//       }
-//     });
-//   };
-
-// GIVEN a note-taking application
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-// WHEN I enter a new note title and the note’s text
-// THEN a Save icon appears in the navigation at the top of the page
-// WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
-// GET /notes should return the notes.html file 
-// get * should return the index.html file 
-// GET /api/notes should read the db.json file and return all saved notes as JSON
-// POST /api/notes should receive a new note to save on teh requst body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages taht could do this for you).
-
-// app.delete('/api/notes/:id', (req, res) => {
-//   const data = fs.readFileSync('./db/db.json', 'utf8');
-//   const notes = JSON.parse(data).filter(note => note.id === req.params.id);
-//   const stringifyedNotes = JSON.stringify(notes, null, 2);
-//   fs.writeFileSync('./db/db.json', stringifyedNotes);
-// });
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT} :rocket:`)
